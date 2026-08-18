c3e314e76fa540dfe5e64a80891fe83e5625a9fb
jjk23 <dlwogh2009@naver.com>
2026-08-18 11:58:37 +0900

    cs파일복구

---
+++ Assets/Script/Anomaly/MoveAno.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveAno : MonoBehaviour
+{
+    [SerializeField] GameObject MoveObj;
+    [SerializeField] GameObject FightObj;
+    [SerializeField] bool isMoveX;//체크하면 좌우이동, 아니면 상하이동
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnEnable()
+    {
+        FightObj.transform.localScale = Vector3.one;
+        if (isMoveX)
+            StartCoroutine("Move");
+        else
+            StartCoroutine("Move2");
+    }
+    IEnumerator Move()
+    {
+        int x = Random.Range(-350, 350);
+        transform.DOLocalMoveX(x, 0.2f);
+        yield return new WaitForSeconds(0.3f);
+        StartCoroutine("Move");
+    }
+    IEnumerator Move2()
+    {
+        int y = Random.Range(-100, 100);
+        transform.DOLocalMoveY(y, 0.2f);
+        yield return new WaitForSeconds(0.3f);
+        StartCoroutine("Move2");
+    }
+
+    public void YachaStart()
+    {
+        MoveObj.SetActive(false);
+        FightObj.SetActive(true);
+        FightObj.transform.DOScale(5, 10);
+    }
+}
---
+++ Assets/Script/Anomaly/Reputation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Reputation : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI RepText;
+    [SerializeField] int reputation;
+    [SerializeField] Image anoImg;
+    [SerializeField] Sprite anospire;
+    public bool isMinusRep;//true면 평판이 까임
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        reputation = 100;
+        StartCoroutine("Change");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RepText.text = "평판 : " + reputation;
+    }
+    IEnumerator Change()
+    {
+        yield return new WaitForSeconds(5f);
+        anoImg.sprite = anospire;
+        StartCoroutine("RepCo");
+    }
+    IEnumerator RepCo()
+    {
+        if (isMinusRep) reputation--;
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine("RepCo");
+    }
+}
